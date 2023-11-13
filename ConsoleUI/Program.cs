b10477767cd6ef73ae38ff6259b3704249b32c23
enesttn <83828133+enesttn@users.noreply.github.com>
2023-11-13 12:50:34 +0300

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,11 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+
+
+foreach (var product in productManager.GetByUnitPrice(10))
+{
+    Console.WriteLine(product.ProductName);
+}
