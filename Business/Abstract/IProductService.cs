0cb43ef54bc759114dec42b3d57aa3d26a139bd6
enesttn <83828133+enesttn@users.noreply.github.com>
2023-11-14 00:58:45 +0300

    IProductService.cs

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -5,8 +5,8 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAllByCategoryId();
-        List<Product> GetByUnitPrice(int id);
-        List<Product> GetByUnitPrice(decimal min, decimal max);
+        List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int id);
+        List<Product> GetByUnitPrice(decimal min, decimal max);               
     }
 }
